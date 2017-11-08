fe47f41c8696a8ba7eac1692f44df075fba0c0fc
Romiver <romina.verdier@gmail.com>
2017-11-07 21:06:51 -0300

    AltaSerie 1.1

---
+++ src/main/java/com/tsijee01/persistence/repository/CapituloRepository.java
@@ -0,0 +1,11 @@
+package com.tsijee01.persistence.repository;
+
+import org.springframework.stereotype.Repository;
+
+import com.tsijee01.persistence.model.CapituloSerie;
+
+@Repository
+public interface CapituloRepository extends BaseRepository<CapituloSerie , Long >{
+
+	
+}
